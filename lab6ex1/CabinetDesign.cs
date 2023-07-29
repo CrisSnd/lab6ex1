ae8b67659786072705215055ab34bf72eabe533e
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-29 15:22:25 +0300

    Add project files.

---
+++ lab6ex1/CabinetDesign.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6ex1
+{
+    class CabinetDesign
+    {
+
+        //proprietati 
+       private int lenght;
+       private int width;
+       private int height;
+
+
+
+        public CabinetDesign(int lenght, int width, int height)
+        {
+
+            this.lenght = lenght;
+            this.width = width;
+            this.height = height;
+        }
+
+
+        public int VolumCalc()
+        {
+            return lenght * width* height;
+        }
+    }
+ }
+
+ 
+
+
---
+++ lab6ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿using lab6ex1;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*Un dulap are trei caracteristici : lungime, latime, inaltime. Dulapul va avea o metoda care va calcula volumul acestuia.
+              Folositi o clasa care va modela un dulap.
+              Clasa va avea trei campuri private intregi: lungime, latime, inaltime.
+              Cele trei campuri vor fi initializate cu ajutorul constructorului.
+              Scrieti o metoda care va calcula volumul dulapului pe baza celor trei campuri.
+              Instantiati un dulap in functia Main, apelati metoda de calcul a volumului si afisati rezultatul.*/
+
+
+
+               CabinetDesign cabinet = new CabinetDesign(300,150,250);
+
+               Console.WriteLine($"The volum of the cabinet is {cabinet.VolumCalc()} cm3");
+       
+        }
+
+     }
+ }
+
